0a25a1d7db770824c277081e218a6974b8cd3762
SauravShandilyaa <saurav.shandilya_cs24@gla.ac.in>
2025-04-09 14:29:50 +0530

    upload assignment

---
+++ assignment.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include "head.h"
+
+int main() 
+{
+    int number;                                     //leap year 
+    printf("Check Leap Year\n");
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    
+    leap(number) ? printf("%d leap year\n", number) : printf("%d not a leap year\n", number);
+
+
+    int num;                                     //armstrong
+    printf("palindrome\n");
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    
+    palin(num) ? printf("%d palindrome\n", num) : printf("%d not palindrome\n", num);
+    return 0;
+}
